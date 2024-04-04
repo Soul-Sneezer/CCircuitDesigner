4865fb61e07de693aca6d1904e4bdf1a5762f808
Soul Sneezer <petrerobertcristian@gmail.com>
2024-04-04 16:34:17 +0300

    Minor changes

--- main.cpp
+++ main.cpp
@@ -4,49 +4,57 @@
 class CircuitElement
 {
 private:
-    int powerIn;
-    int powerOut;
+    std::pair<int, int> position;
+
+    int voltageIn;
+    int voltageOut;
+    int temperature; // useless for now
+
+    CircuitElement* in; // prev element(may be NULL)
+    CircuitElement* out; // next element
 public:
     CircuitElement()
     {
-        this->powerIn = 0;
-        this->powerOut = 0;
+        this->voltageIn = 0;
+        this->voltageOut = 0;
+        this->in = NULL;
+        this->out = NULL;
     }
 
     CircuitElement(int value)
     {
-        this->powerIn = value;
-        this->powerOut = value;
+        this->voltageIn = value;
+        this->voltageOut = value;
     }
 
     CircuitElement(int in, int out)
     {
-        this->powerIn = in;
-        this->powerOut = out;
+        this->voltageIn = in;
+        this->voltageOut = out;
     }
 
     CircuitElement(const CircuitElement& element)
     {
-        this->powerIn = element.powerIn;
-        this->powerOut = element.powerOut;
+        this->voltageIn = element.powerIn;
+        this->voltageOut = element.powerOut;
     }
 
     ~CircuitElement()
     {
     }
 
-    int getPowerOut() { return this->powerOut; };
+    int getPowerOut() { return this->voltageOut; };
 
     friend std::ostream& operator<<(std::ostream& os, const CircuitElement& el)
     {
-        os << "Power in: "<<el.powerIn <<"    Power out: " <<el.powerOut <<"\n";
+        os << "Power in: "<<el.voltageIn <<"    Power out: " <<el.powerOut <<"\n";
         return os;        
     }
 
     CircuitElement& operator=(const CircuitElement& element)
     {
-        this->powerIn = element.powerIn;
-        this->powerOut = element.powerOut;
+        this->voltageIn = element.powerIn;
+        this->voltageOut = element.powerOut;
 
         return *this;
     }
@@ -97,19 +105,28 @@ class Resistor : CircuitElement
 {
 private:
     int resistance;
+    int powerDissipation;
+    int tolerance;
 public:
     Resistor()
     {
+        this->resistance = 0;
+        this->powerDissipation = 0;
+        this->tolerance = 0;
     }
 
     Resistor(int resistance) 
     {
         this->resistance = resistance;
+        this->powerDissipation = 0;
+        this->tolerance = 0;
     }
 
     Resistor(const Resistor& r)
     {
         this->resistance = r.resistance;
+        this->powerDissipation = r.powerDissipation;
+        this->tolerance = tolerance;
     }
 
     ~Resistor()
@@ -118,13 +135,15 @@ public:
 
     friend std::ostream& operator<<(std::ostream& os, const Resistor& el)
     {
-        os<<"Resistance: "<<el.resistance<<"\n";
-        return os;
+        os<<"Resistance: "<<el.resistance<<"    Power that can be safely dissipated: "<<el.powerDissipation<<"   Tolerance: "<<el.tolerance<<"\n";
+        return os; 
     }
 
     Resistor& operator=(const Resistor& resistor)
     {
         this->resistance = resistor.resistance;
+        this->powerDissipation = resistor.powerDissipation;
+        this->tolerance = resistor.tolerance;
         return *this;
     }
 };
@@ -135,10 +154,19 @@ private:
     std::pair<int, int> start;
     std::pair<int, int> end;
 
+    int resistance;
     bool reverse;
 public:
     Cable()
     {
+        this->start.first = 0;
+        this->start.second = 0;
+
+        this->end.first = 0;
+        this->end.second = 0;
+
+        this->resistance = 0;
+        this->reverse = 0;
     }
 
     Cable(std::pair<int, int> start, std::pair<int, int> end) 
@@ -149,7 +177,10 @@ public:
 
     Cable(const Cable& c)
     {
-
+        this->start = c.start;
+        this->end = c.end;
+        this->resistance = c.resistance;
+        this->reverse = c.reverse;
     }
 
     ~Cable()
@@ -167,6 +198,7 @@ public:
         this->start = cable.start;
         this->end = cable.end;
         this->reverse = cable.reverse;
+        this->resistance = cable.resistance;
 
         return *this;
     }
@@ -180,7 +212,7 @@ public:
     {
     }
     
-    Source(int power)  : CircuitElement(power)
+    Source(int voltage)  : CircuitElement(voltage)
     {
     }
 
@@ -203,22 +235,57 @@ public:
     }
 };
 
+
+class Battery
+{
+    private:
+    public:
+    Battery()
+    {
+    }
+
+    Baterry(const Battery& b)
+    {
+    }
+
+    ~Battery()
+    {
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const Source& el)
+    {
+        return os;
+    }
+
+    Source& operator=(const Source& source)
+    {
+        return *this;
+    }
+}
+
 class Circuit
 {
 private:
-    int powerIn;
-    int powerOut;
+    int voltageIn;
+    int voltageOut;
 
-    std::vector<CircuitElement> elements; // not suitable for a circuit, replace soon
+    CircuitElement* in;
+    CircuitElement* out;
 public:
     Circuit()
     {
-
+        this->voltageIn = 0;
+        this->voltageOut = 0;        
+        this->in = NULL;
+        this->out = NULL;
     }
 
     Circuit(const Circuit& circuit)
     {
-        this->elements = circuit.elements;
+        this->in = circuit.in;
+        this->out = circuit.out;
+        this->voltageIn = circuit.voltageIn;
+        this->voltageOut = circuit.voltageOut;
     }
 
     ~Circuit()
@@ -233,8 +300,6 @@ public:
 
     Circuit& operator=(const Circuit& circuit)
     {
-        this->elements = circuit.elements;
-
         return *this;
     }
     
@@ -242,7 +307,11 @@ public:
 
     void addElemToCircuit(CircuitElement* element)
     {
-        getElements().push_back(*element);
+    }
+
+    void newInputElem(CircuitElement* element)
+    {
+        this->in = element;
     }
 
     void removeElemFromCircuit(const int index)
